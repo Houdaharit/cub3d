3eff28614408fb05dfd87660bf6bf63bddd07c0a
Houda Harit <houdaharit20@gmail.com>
2023-02-01 04:48:56 +0100

    fuck it

--- intersection.c
+++ intersection.c
@@ -6,7 +6,7 @@
 /*   By: hharit <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 05:10:03 by hharit            #+#    #+#             */
-/*   Updated: 2023/02/01 04:11:51 by hharit           ###   ########.fr       */
+/*   Updated: 2023/02/01 04:23:58 by hharit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	check_grid(t_cub3d cub, int x, int y, char dir)
 	else if (dir == 'V' && (!(cub.ray.ray_angle < M_PI_2 || cub.ray.ray_angle < (1.5 * M_PI))))
 		x--;
 	if (x < 0 || x > cub.mlx.width || y < 0 || y > cub.mlx.height)
-		return (1);
+		return (0);
 	grid_x = floor(x / cub.tile_size);
 	grid_y = floor(y / cub.tile_size);
 	return (cub.map[grid_x][grid_y] == '1');
